9e060d11b174946da23e1af08332b860f8f05e25
hhlee <60291830+cryptogus@users.noreply.github.com>
2024-07-19 13:58:38 +0900

    create tvla data set

---
+++ Case2FixedVsRandomKey.py
@@ -0,0 +1,29 @@
+from cwtvla.ktp import FixedVRandomKey, verify_AES
+import numpy as np
+import pandas as pd
+
+N = 10000
+key_len = 16
+ktp = FixedVRandomKey(key_len)
+
+fixedKeyList = []
+fixedPtList = []
+randomKeyList = []
+randomPtList = []
+
+for i in range(N):
+	key, text = ktp.next_group_A() # Fixed Data
+	fixedKeyList.append(key.hex())
+	fixedPtList.append(text.hex())
+
+	key, text = ktp.next_group_B() # Random Data
+	randomKeyList.append(key.hex())
+	randomPtList.append(text.hex())
+
+fixedSet = {"Key":fixedKeyList, "Plaintext":fixedPtList}
+fixedDf = pd.DataFrame(fixedSet)
+fixedDf.to_csv("Case2FixedKeyDataSet.csv")
+
+randomSet = {"Key":randomKeyList, "Plaintext":randomPtList}
+randomDf = pd.DataFrame(randomSet)
+randomDf.to_csv("Case2RandomKeyDataSet.csv")
